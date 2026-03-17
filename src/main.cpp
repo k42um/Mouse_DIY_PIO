024e32dcc9dfa348ca6a6b5f2e46432b906818c7
k42um <kazuma041016@icloud.com>
2026-03-18 00:19:37 +0900

    基礎骨格は完成

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,15 @@
 #define X_AXIS_PIN A0
 #define Y_AXIS_PIN A1
 
-#define JOY_SENSITIVITY 15
+#define VARIABLE_RESISTER_PIN A2
+
+#define JOY_SENSITIVITY_MAX 30
+#define JOY_SENSITIVITY_MIN 1
+
 #define JOY_CENTER_THRESHOLD 20
+
+#define ANALOG_READ_MAX 1023
+#define ANALOG_READ_MIN 0
 
 void setup(){
     pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
@@ -23,6 +30,9 @@ void setup(){
 
 void loop(){
     //FIXME: 一回ボタンを押しただけでも、複数クリック判定になってしまう。
+
+    int varResVal = analogRead(VARIABLE_RESISTER_PIN);
+    uint8_t joySensitivity = map(varResVal, ANALOG_READ_MIN, ANALOG_READ_MAX, JOY_SENSITIVITY_MIN, JOY_SENSITIVITY_MAX);
 
     // 左ボタン
     if(!digitalRead(LEFT_BUTTON_PIN) && !Mouse.isPressed(MOUSE_LEFT)){ 
@@ -60,7 +70,7 @@ void loop(){
     if(abs(xMove) < JOY_CENTER_THRESHOLD) xMove = 0;
     if(abs(yMove) < JOY_CENTER_THRESHOLD) yMove = 0;
 
-    Mouse.move(xMove / JOY_SENSITIVITY, yMove / JOY_SENSITIVITY, 0);
+    Mouse.move(xMove / joySensitivity, yMove / joySensitivity, 0);
 }
 
 // 参考: https://speeddemon.jp/mouse-h/
